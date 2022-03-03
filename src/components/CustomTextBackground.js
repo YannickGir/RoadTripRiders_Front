3dd693e91f44c823c387808815e34f448d366029
Yannick.C <girofare@hotmail.com>
2022-03-03 16:08:27 +0100

    pages creation roadtrip créées 1

---
+++ src/components/CustomTextBackground.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { View, SafeAreaView, StyleSheet, TextInput, Text } from "react-native";
+import { Input } from "react-native-elements";
+import CustomButtonModif from "../components/CustomButtonModif";
+
+// On crée un composant qui pourra être injecté partout dans l'application car nous avons de nombreux inputs
+// on passe en argument toutes les valeurs que l'on va changer d'un input à l'autre
+const CustomTextBackground = ({ text1, text2 }) => {
+  return (
+    <View>
+      <SafeAreaView style={styles.text}>
+        <Text>{text1}</Text>
+        <Text style={{ color: "#ff8b00" }}>{text2}</Text>
+        <View>
+          <CustomButtonModif />
+        </View>
+      </SafeAreaView>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {},
+  text: {
+    backgroundColor: "#FFEDAC",
+    borderRadius: 30,
+    paddingHorizontal: 5,
+    // paddingVertical: 5,
+    marginVertical: 5,
+    width: 300,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+});
+
+export default CustomTextBackground;
--- src/screens/CreateRoadTripScreenFirstStep.js
+++ src/screens/CreateRoadTripScreenFirstStep.js
@@ -16,6 +16,9 @@ import CustomTimeNewTripInput2 from "../components/CustomTimeNewTripInput2";
 import CustomButtonOrange from "../components/CustomButtonOrange";
 import CustomButtonOrangeNext from "../components/CustomButtonOrangeNext";
 import CustomHeader from "../components/CustomHeader";
+import CustomButtonValidation from "../components/CustomButtonValidation";
+import CustomButtonChoice from "../components/CustomButtonChoice";
+import CustomButtonChoiceValidate from "../components/CustomButtonChoiceValidate";
 
 //------------pour barre de progression----nb installé : npm install react-native-step-indicator --save   -----------------------
 import StepIndicator from "react-native-step-indicator";
@@ -63,14 +66,20 @@ export default function CreateRoadTripScreenFirstStep(props) {
   const [formProgress, setFormProgress] = useState(0);
   const [toggleButton, setToggleButton] = useState(false);
 
+  const firstep = "";
+  const secondstep = "";
+
   return (
     //------------------------------------------ FIRST STEP PAGE---------------------
     // <View style={styles.container}>
     //   <CustomHeader
-    //     onPress={() =>
-    //       props.navigation.navigate("Homepage", { screen: "HomeScreen" })
-    //     }
-    //   />
+    //   onPress={() =>
+    //     props.navigation.navigate("RoadtripList", {
+    //       screen: "RoadtripListScreen",
+    //     })
+    //   }
+    //   title="CREE TON TRIP"
+    // />
     //   <View style={styles.barprogress}>
     //     <StepIndicator
     //       customStyles={customStyles}
@@ -179,49 +188,40 @@ export default function CreateRoadTripScreenFirstStep(props) {
       <View style={styles.barprogress}>
         <StepIndicator
           customStyles={customStyles}
-          currentPosition={formProgress}
+          currentPosition={2}
           stepCount={3}
         />
       </View>
-
-      <View style={styles.switch}>
-        <Text> Privé </Text>
-        <Switch
-          trackColor={{ false: "#363432", true: "teal" }}
-          thumbColor="#FF8B00"
-          ios_backgroundColor="#FEFAEA"
-          onValueChange={(value) => setToggleButton(value)}
-          value={toggleButton}
-        />
-        <Text> Public </Text>
+      <View style={{ marginTop: "8%" }}>
+        <Text> Type de Roadtrip </Text>
+      </View>
+      <View style={styles.choice}>
+        <CustomButtonChoiceValidate title={"Cool"} />
+        <CustomButtonChoice title={"Sportif"} />
+        <CustomButtonChoice title={"Tourisme"} />
       </View>
 
-      <View style={{ paddingBottom: 10, paddingTop: 10 }}>
+      <View
+        style={{
+          marginBottom: "5%",
+          marginTop: "10%",
+          alignItems: "center",
+        }}
+      >
+        <Text> Pour quel type de moto ? </Text>
         <CustomTimeNewTripInput2
-          placeholder="Titre de votre Roadtrip"
+          placeholder="Toutes catégories"
           value={event_title}
           setValue={setEvent_title}
         />
-        <CustomTimeNewTripInput2
-          placeholder="Date de départ"
-          value={date_sortie}
-          setValue={setDate_sortie}
-        />
       </View>
 
-      <Text style={{ paddingTop: 5 }}>Horaires :</Text>
-      <View style={styles.horaires}>
+      <View style={styles.tailleGroupe}>
         <View style={{ alignItems: "center" }}>
-          <Text>Départ :</Text>
-          <CustomTimeNewTripInput
-            placeholder="9:00"
-            value={date_sortie}
-            setValue={setDate_sortie}
-          />
+          <Text>Taille du groupe :</Text>
         </View>
         <Text> </Text>
         <View style={{ alignItems: "center" }}>
-          <Text>Arrivée :</Text>
           <CustomTimeNewTripInput
             placeholder="16:00"
             value={date_sortie}
@@ -238,37 +238,20 @@ export default function CreateRoadTripScreenFirstStep(props) {
           marginTop: "5%",
         }}
       >
-        <View style={{ marginBottom: "3%" }}>
-          <CustomButtonOrange
-            title="NOUVEL ITINERAIRE"
-            onPress={() => props.navigation.navigate("newRoadTripFirstStep")}
-          />
-          <CustomButton
-            title="ITINERAIRE PROPOSE"
-            onPress={() => props.navigation.navigate("newRoadTripFirstStep")}
-          />
-        </View>
-        <View style={{ marginTop: "15%" }}>
-          <Text>Aucun itinéraire choisit ou créé</Text>
-        </View>
-      </View>
-
-      {/* FLECHE PAGE SUIVANTE */}
-      <View style={styles.bottomPage}>
-        <View style={{ marginHorizontal: "40%" }}></View>
-        <View style={{ marginTop: "10%", marginBottom: "5%" }}>
-          <CustomButtonOrangeNext
-            title="NOUVEL ITINERAIRE"
-            onPress={
-              (() => props.navigation.navigate("newRoadTripFirstStep"),
-              () => setFormProgress(1))
+        <View style={styles.bottomPage3}>
+          <CustomButtonValidation
+            title="VALIDER !"
+            onPress={() =>
+              props.navigation.navigate("CreateRoadTripRecap", {
+                screen: "CreateRoadTripScreenRecap",
+              })
             }
           />
         </View>
       </View>
     </View>
 
-    //------------------------------------------ END OF SECOND STEP PAGE---------------------
+    //------------------------------------------ END OF THIRD STEP PAGE---------------------
   );
 }
 // onPageChange(position);
@@ -281,6 +264,16 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "center",
   },
+  choice: {
+    width: "70%",
+    alignItems: "center",
+    flexDirection: "row",
+    justifyContent: "center",
+    height: "4%",
+    marginBottom: "5%",
+    marginTop: "2%",
+    justifyContent: "space-between",
+  },
 
   bottomPage: {
     width: deviceWidth,
@@ -291,10 +284,20 @@ const styles = StyleSheet.create({
     // marginTop: "10%",
   },
 
+  bottomPage3: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "row",
+    maxHeight: 80,
+    marginTop: "30%",
+  },
+
   barprogress: {
     width: deviceWidth,
     backgroundColor: "#FEFAEA",
-    paddingTop: 15,
+    paddingTop: "3%",
+    marginBottom: "3%",
   },
   container: {
     width: deviceWidth,
@@ -314,7 +317,16 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     flexDirection: "row",
     justifyContent: "space-between",
-    backgroundColor: "white",
     maxHeight: 80,
+  },
+
+  tailleGroupe: {
+    flex: 2,
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    maxHeight: "10%",
+    marginTop: "10%",
   },
 });
