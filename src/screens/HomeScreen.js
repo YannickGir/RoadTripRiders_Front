c33a26adee044f16007b58000a7bce878e6ec3a2
Allan S <allanxgamiing@gmail.com>
2022-03-04 19:00:44 +0100

    front qui fonctionne

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,30 +1,95 @@
-import React from "react";
-import { StyleSheet, Button, View, Text, Icon } from "react-native";
-
+import React, { useEffect, useState, useRef } from "react";
+import {
+  StyleSheet,
+  Button,
+  View,
+  Text,
+  Icon,
+  Image,
+  ScrollView,
+  Dimensions,
+} from "react-native";
+import { MA_VARIABLE } from "@env";
+import { Card } from "react-native-elements";
+import { FontAwesome, Ionicons } from "@expo/vector-icons";
+import CustomHeader from "../components/CustomHeader";
+import CustomInput from "../../src/components/CustomInput";
+let deviceHeight = Dimensions.get("window").height;
+let deviceWidth = Dimensions.get("window").width;
 export default function HomepageScreen(props) {
+  const [roadTripList, setRoadTripList] = useState([]);
+  useEffect(() => {
+    async function loadRoadTrip() {
+      const data = await fetch(
+        `https://roadtripsriders3.herokuapp.com/roadtriplist`
+      );
+      var body = await data.json();
+      console.log("body", body);
+
+      setRoadTripList(
+        body.map((tripData, i) => {
+          return (
+            <Card
+              key={i}
+              containerStyle={{ backgroundColor: "#FFEDAC", width: "100%" }}
+            >
+              <View>
+                <Image style={styles.avatar} source={{}} />
+                <Text></Text>
+              </View>
+              <View>
+                <Text>{tripData.event_title}</Text>
+                <Text>Stars</Text>
+              </View>
+              <Image />
+              <View>
+                <Text>Distancejhiokmhfmu</Text>
+                <Text>km</Text>
+              </View>
+              <View>
+                <Text>Durée :</Text>
+                <Text>H</Text>
+              </View>
+              <View>
+                <Text>Niveau :</Text>
+                <Text>cool</Text>
+              </View>
+            </Card>
+          );
+        })
+      );
+    }
+
+    loadRoadTrip();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Homepage Screen</Text>
-      <Button
-        icon={<Icon name="arrow-right" size={20} color="#eb4d4b" />}
-        title="go to itinerary !"
-        type="solid"
-        onPress={() =>
-          props.navigation.navigate("Itinerary", {
-            screen: "ItineraryScreen",
-          })
-        }
-      />
-      <Button
-        icon={<Icon name="arrow-right" size={20} color="#eb4d4b" />}
-        title="go to roadtripList !"
-        type="solid"
-        onPress={() =>
-          props.navigation.navigate("RoadtripList", {
-            screen: "RoadtripListScreen",
-          })
-        }
-      />
+      <ScrollView style={{ flex: 1, width: "100%" }}>
+        {roadTripList}
+
+        <Text>Homepage Screen</Text>
+        <Button
+          icon={<Icon name="arrow-right" size={20} color="#eb4d4b" />}
+          title="go to itinerary !"
+          type="solid"
+          onPress={() =>
+            props.navigation.navigate("Itinerary", {
+              screen: "ItineraryScreen",
+            })
+          }
+        />
+        <Button
+          icon={<Icon name="arrow-right" size={20} color="#eb4d4b" />}
+          title="go to roadtripList !"
+          type="solid"
+          onPress={() =>
+            props.navigation.navigate("RoadtripList", {
+              screen: "RoadtripListScreen",
+            })
+          }
+        />
+      </ScrollView>
     </View>
   );
 }
@@ -35,5 +100,6 @@ const styles = StyleSheet.create({
     backgroundColor: "#FEFAEA",
     alignItems: "center",
     justifyContent: "center",
+    width: deviceWidth,
   },
 });
