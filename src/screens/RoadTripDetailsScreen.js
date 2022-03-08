8fafa09dbbb94529e27b021a3da8e679ecab207e
Lili Liardet <liardet.lili@gmail.com>
2022-03-08 18:00:16 +0100

    avec front sortie

--- src/screens/RoadTripDetailsScreen.js
+++ src/screens/RoadTripDetailsScreen.js
@@ -14,6 +14,7 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import {
   AntDesign,
   FontAwesome,
+  FontAwesome5,
   MaterialCommunityIcons,
   Ionicons,
 } from '@expo/vector-icons';
@@ -21,6 +22,7 @@ import {
 import { MA_VARIABLE } from '@env';
 import CustomInputWithoutPlaceholder from '../components/CustomInputWithoutPlaceholder';
 import CustomLongInputWithoutPlaceholder from '../components/CustomLongInputWithoutPlaceholder';
+import CustomInputTimeWithoutPlaceholder from '../components/CustomInputTimeWithoutPlaceholder';
 import CustomButton from '../components/CustomButton';
 let deviceHeight = Dimensions.get('window').height;
 let deviceWidth = Dimensions.get('window').width;
@@ -69,7 +71,7 @@ const RoadTripDetailsScreen = (props) => {
               width: 340,
               borderRadius: 15,
               margin: 10,
-              marginTop: '10%',
+              marginTop: '5%',
             }}
           >
             <View style={{ flexDirection: 'row' }}>
@@ -93,31 +95,176 @@ const RoadTripDetailsScreen = (props) => {
                 <Text style={{ fontSize: 20 }}>
                   La sortie de Prénom à ajouter
                 </Text>
-                <Text>
-                  <FontAwesome name='motorcycle' size={30} color='#363432' />{' '}
-                  Titre de la sortie
-                </Text>
+                <Text>Titre de la sortie</Text>
               </View>
             </View>
           </View>
         </View>
+        <View style={styles.centered}>
+          <Text style={{ paddingTop: '2%', paddingBottom: '2%' }}>
+            Un peu plus de détail sur ce Roadtrip
+          </Text>
+          <CustomLongInputWithoutPlaceholder />
+          <Text style={{ paddingTop: '2%', paddingBottom: '4%' }}>
+            L'itinéraire
+          </Text>
+          <Image
+            source={{
+              uri: 'https://res.cloudinary.com/la-capsule-batch-49/image/upload/v1646669835/m4fhqwiqunvjcidxvfg2.jpg',
+            }}
+            style={{
+              width: 340,
+              height: 250,
+              alignContent: 'center',
+              alignItems: 'center',
+              borderRadius: 15,
+            }}
+          />
+          <View
+            style={{
+              flex: 1,
+              alignItems: 'center',
+              justifyContent: 'space-around',
+              flexDirection: 'row',
+              backgroundColor: '#FFD230',
+              padding: 10,
+              height: 80,
+              width: 340,
+              borderRadius: 15,
+              margin: 10,
+              marginTop: '2%',
+            }}
+          >
+            <View>
+              <Text>Distance :</Text>
+              <Text
+                style={{
+                  alignSelf: 'center',
+                  fontWeight: 'bold',
+                  fontSize: 15,
+                }}
+              >
+                XX km
+              </Text>
+            </View>
+            <View>
+              <Text>Durée :</Text>
+              <Text
+                style={{
+                  alignSelf: 'center',
+                  fontWeight: 'bold',
+                  fontSize: 15,
+                }}
+              >
+                XX h
+              </Text>
+            </View>
+            <View>
+              <Text>Niveau :</Text>
+              <Text
+                style={{
+                  alignSelf: 'center',
+                  fontWeight: 'bold',
+                  fontSize: 15,
+                }}
+              >
+                Cool
+              </Text>
+            </View>
+          </View>
+          <Text style={{ paddingTop: '2%', paddingBottom: '2%' }}>
+            <FontAwesome5 name='flag' size={24} color='#363432' /> Départ
+          </Text>
+          <CustomInputWithoutPlaceholder />
+          <Text style={{ paddingTop: '2%', paddingBottom: '2%' }}>
+            <FontAwesome5 name='flag-checkered' size={24} color='#363432' />{' '}
+            Arrivée
+          </Text>
+          <CustomInputWithoutPlaceholder />
+          <Text style={{ paddingTop: '2%' }}>
+            <FontAwesome5 name='clock' size={24} color='#363432' /> Horaires
+          </Text>
+        </View>
+        <View style={styles.secondaryTime}>
+          <FontAwesome5 name='flag' size={24} color='#363432' />
+          <CustomInputTimeWithoutPlaceholder />
+          <FontAwesome5 name='flag-checkered' size={24} color='#363432' />
+          <CustomInputTimeWithoutPlaceholder />
+        </View>
+        <View style={styles.secondary}>
+          <View style={styles.button}>
+            <FontAwesome
+              name='group'
+              size={60}
+              color='#363432'
+              style={{ paddingTop: 20, paddingBottom: 10 }}
+            />
+            <Text>Taille du Groupe</Text>
+            <Text>XX</Text>
+          </View>
+          <View
+            style={styles.button}
+            onPress={() =>
+              props.navigation.navigate('UserInfosEdition', {
+                screen: 'UserInfosEditionScreen',
+              })
+            }
+          >
+            <FontAwesome5
+              name='hand-peace'
+              size={60}
+              color='#363432'
+              style={{ paddingTop: 20, paddingBottom: 10 }}
+            />
+
+            <Text>Places restantes</Text>
+            <Text>XX</Text>
+          </View>
+        </View>
+        <View style={styles.centered}>
+          <Text style={{ paddingTop: '2%', paddingBottom: '2%' }}>
+            <FontAwesome name='motorcycle' size={24} color='#363432' />{' '}
+            Catégorie de Motos
+          </Text>
+          <CustomInputWithoutPlaceholder />
+        </View>
+        <CustomButton title="S'INSCRIRE" />
       </ScrollView>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#FFEDAC',
+    padding: 10,
+    height: 160,
+    width: 160,
+    borderRadius: 15,
+    margin: 10,
+  },
   container: {
-    width: deviceWidth,
-    height: deviceHeight,
+    flex: 1,
     backgroundColor: '#FEFAEA',
     alignItems: 'center',
     justifyContent: 'center',
-    marginTop: 0,
-    paddingTop: 0,
   },
   secondary: {
     flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  secondaryTime: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    paddingLeft: '15%',
+    paddingRight: '15%',
+  },
+  centered: {
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   //style pour le header
   headerContainer: {
